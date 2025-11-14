e8307275c271f2f61bfde4e9fed58227117b5f23
Jerome Tonnelier <jerome.tonnelier@ogury.co>
2025-11-14 09:29:12 +0100

    WIP

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "OguryGoogleMobileAdsAdapter",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "OguryGoogleMobileAdsAdapter",
+            targets: ["OguryGoogleMobileAdsAdapter"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Ogury/ogury-sdk-spm", branch: "release/5.2.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads", from: "12.13.0"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "OguryMediationGoogleMobileAds",
+            url: "https://binaries.ogury.co/internal/prod/OguryMediationGoogleMobileAds/OguryMediationGoogleMobileAds-Prod-5.2.0.zip",
+            checksum: "57e98128af699ad27d1ddb9c4936821aef37c5df3b776f8d52fbd38b5768edd7"
+        ),
+        .target(
+            name: "OguryGoogleMobileAdsAdapter",
+            dependencies: [
+                "OguryMediationGoogleMobileAds",
+                .product(name: "OgurySdk", package: "ogury-sdk-spm"),
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
+            ]
+        )
+    ]
+)
